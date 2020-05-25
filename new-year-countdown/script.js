0dc4d839b112a128a6d15d416660bf317fdf02d7
Juan Daniel <juan@mctekk.com>
2020-05-24 20:09:43 -0400

    new year countdown

---
+++ new-year-countdown/script.js
@@ -0,0 +1,36 @@
+  
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const countdown = document.getElementById('countdown');
+const year = document.getElementById('year');
+const loading = document.getElementById('loading');
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+year.innerText = newYearTime.getFullYear();
+
+function updateCountdown() {
+    const currenntTime = new Date();
+    const diff = newYearTime - currenntTime;
+
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000 ) % 60;
+
+    days.innerText = d;
+    hours.innerText = h < 10 ? "0" + h : h;
+    minutes.innerText = m < 10 ? "0" + m : m;
+    seconds.innerText = s < 10 ? "0" + s : s;
+}
+
+setTimeout(() => {
+    loading.remove();
+    countdown.style.display = "flex";
+}, 1000);
+
+setInterval(() => updateCountdown(), 1000);
